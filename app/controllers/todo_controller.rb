5aee81889970c9b93eca2a648ea25a3c706f1ac5
Jennifer Pillajo <jennifer.pillajo1705@gmail.com>
2017-10-22 15:46:18 +0000

    add new todo view

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,34 +4,8 @@ class TodoController < ApplicationController
 
     def show
         @my_todo =Todo.find_by_id(params[:id])
-        # if params[:id] == "1"
-        #     @todo_description = "Make the curriculum"
-        #     @todo_pomodoro_estimate = 5
-       
-        # elsif params[:id] =="2"
-        #     @todo_description="buy workshop supplies"
-        #     @todo_pomodoro_estimate = 3
-       
-        # elsif params[:id] =="3"
-        #     @todo_description ="meet with the volunteer trainers"
-        #     @todo_pomodoro_estimate = 2
-        
-        # elsif params[:id] =="4"
-        #     @todo_description ="order food for saturday and sunday"
-        #     @todo_pomodoro_estimate = 1
-        
-        # elsif params[:id] =="5"
-        #     @todo_description ="check prework assigment"
-        #     @todo_pomodoro_estimate = 2
-            
-        
-        # elsif params[:id] =="6"
-        #     @todo_description ="send work location to all students"
-        #     @todo_pomodoro_estimate = 1
-        
-        # elsif params[:id] =="7"
-        #     @todo_description ="have a great workshop"
-        #     @todo_pomodoro_estimate = 0
-        # end
+    end
+    
+    def new
     end
 end
